dc597ea9b2076d2ce64ae014b13f06496eeff1f6
Zeragamba <Zera@zeragamba.ca>
2022-05-17 23:15:47 -0400

    Make LED_OFF a bit eaiser to access

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,15 @@
 import DiceSet from './src/dice-set';
 import Die from './src/die';
-import * as LedColor from './src/led-color';
+import LedColor, { LedOff, LedRgb } from './src/led-color';
 
 export const diceSet: DiceSet;
+const LED_OFF = LedColor.OFF;
 
 export {
   Die,
   LedColor,
+  LED_OFF,
+
+  LedRgb,
+  LedOff,
 };
--- src/led-color.d.ts
+++ src/led-color.d.ts
@@ -1,5 +1,11 @@
-export type LedColour = [red: number, green: number, blue: number] | [off: 0]
+export type LedRgb = [red: number, green: number, blue: number];
+export type LedOff = [0]
 
-const LedColor: Record<string, LedColour>;
+const LedColor = {
+  OFF: LedOff,
+  RED: LedRgb,
+  GREEN: LedRgb,
+  BLUE: LedRgb,
+};
 
 export default LedColor;
