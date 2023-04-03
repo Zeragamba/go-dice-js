c98212eb4aa338ec6ba95ecc5fd8d5199188b865
Carl McGee <carl.jack.mcgee@gmail.com>
2023-04-02 20:26:59 -0500

    Update dice-set.d.ts

--- packages/go-dice-api/src/dice-set.d.ts
+++ packages/go-dice-api/src/dice-set.d.ts
@@ -6,6 +6,6 @@ declare const GoDice: any;
 export default class DiceSet extends EventEmitter {
   requestDie (): void
 
-  on (event: "connected", handler: (die: Die) => void)
-  on (event: "reconnected", handler: (die: Die) => void)
+  on (event: 'connected', handler: (die: Die) => void)
+  on (event: 'reconnected', handler: (die: Die) => void)
 }
